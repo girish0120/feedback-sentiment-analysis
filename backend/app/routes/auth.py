32295f0ca81c9d283cbf7740f48d669ce18edec5
deepak <deepak_202200256@smit.smu.edu.in>
2023-08-24 09:40:06 +0500

    login endpoint created & search user fn created

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,1 +1,15 @@
-# create a endpoint /login and check user with  (refer /db_handler.py) -> deepak/ishaan
+from flask import jsonify, request
+from app import app, dbConn
+from db_handler import search_user
+
+
+@app.route('/login', methods=["POST"])  # Use 'methods' instead of 'method'
+def login():
+    loginDetails = request.get_json()
+    userId = loginDetails.get("userId")
+    pwd = loginDetails.get("pwd")
+    result = search_user(dbConn, userId, pwd)
+    if not result:
+        return jsonify({"message": "Invalid Credentials"}), 401
+    else:
+        return jsonify({"message": "Login Successful"}),200
